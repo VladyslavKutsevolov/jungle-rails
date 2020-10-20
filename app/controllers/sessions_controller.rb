ca012a8b6f2e6ac2a989a2c63c5c5ce83c070ff3
Vladyslav <kytsevolov@gmail.com>
2020-10-20 15:08:35 -0600

    tested logIn and logOut

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
+    user = User.authenticate_with_credentials(session_params[:email], session_params[:password])
+    if user
       session[:user_id] = user.id
       redirect_to '/'
     else
+      raise 'session user', user
       flash[:error] = 'Invalid credentials'
       redirect_to '/login'
     end
